18d9eb49fe4aa5152e4ef44336c5401ff7a787a9
janiko71 <janiko@geba.fr>
2024-12-02 16:12:46 +0100

    Update mydig.sh

--- mydig.sh
+++ mydig.sh
@@ -2,12 +2,15 @@
 
 # Vérifie si un domaine a été passé en paramètre
 if [ -z "$1" ]; then
-    echo "Usage: $0 <domain>"
+    echo "Usage: $0 <domain> [@dns_server]"
     exit 1
 fi
 
 # Nom de domaine passé en argument
 DOMAIN=$1
+
+# Serveur DNS passé en second argument (optionnel)
+DNS_SERVER=${2:-}
 
 # Liste des types d'enregistrements DNS à interroger
 RECORD_TYPES=("A" "AAAA" "MX" "TXT" "NS" "CNAME" "SOA" "PTR" "SRV")
@@ -15,7 +18,7 @@ RECORD_TYPES=("A" "AAAA" "MX" "TXT" "NS" "CNAME" "SOA" "PTR" "SRV")
 # Boucle sur chaque type d'enregistrement
 for record in "${RECORD_TYPES[@]}"; do
     # Récupère les résultats pour le type actuel
-    results=$(dig +noall +answer "$DOMAIN" "$record")
+    results=$(dig +noall +answer "$DOMAIN" "$record" "$DNS_SERVER")
 
     # Si des résultats existent, les afficher avec un préfixe aligné
     if [ -n "$results" ]; then
